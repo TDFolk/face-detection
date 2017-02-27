fdb74c47aec9a11b1d6db3aead36f705148ebc82
TDFolk <tfolk17@gmail.com>
2017-02-26 21:40:46 -0700

    haar cascade

--- src/facedetection.py
+++ src/facedetection.py
@@ -1,11 +1,23 @@
 import sys
 from src import crop
 import cv2
+import numpy as np
 
-image = sys.argv[1]
+face_cascade = cv2.CascadeClassifier('C:/Users/Trent/Anaconda3/Library/etc/haarcascades/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('C:/Users/Trent/Anaconda3/Library/etc/haarcascades/haarcascade_eye.xml')
+img = cv2.imread(sys.argv[1])
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-image = crop.crop_image(image)
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
+cv2.imwrite('result.jpg',img)
 
 
 
